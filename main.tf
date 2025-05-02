8eb7db2ee98faefdd1822f458e7597d384c4dbca
abbysac <abbysac@gmail.com>
2025-05-02 12:32:09 -0400

    gha policy

--- main.tf
+++ main.tf
@@ -57,7 +57,7 @@ output "csvdata" {
 resource "aws_iam_openid_connect_provider" "github" {
   url             = "https://token.actions.githubusercontent.com"
   client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = ["74F3A68F16524F15424927704C9506F55A9316BD"] # GitHub's current thumbprint
+  thumbprint_list = ["74f3a68f16524f15424927704c9506f55a9316bd"] # GitHub's current thumbprint
 }
 
 resource "aws_iam_role" "github_oidc_role" {
