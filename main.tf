0420bd18f148480229830cdf8675edcb3e24becc
abbysac <abbysac@gmail.com>
2025-05-02 12:00:19 -0400

    gha policy

--- main.tf
+++ main.tf
@@ -54,66 +54,43 @@ output "csvdata" {
 }
 
 
-# 1. OIDC provider for GitHub
-resource "aws_iam_openid_connect_provider" "github" {
-  url             = "https://token.actions.githubusercontent.com"
-  client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = ["74F3A68F16524F15424927704C9506F55A9316BD"] # GitHub's current thumbprint
-}
-
-
-# 2. IAM Role for GitHub Actions
 resource "aws_iam_role" "github_oidc_role" {
   name = "GitHubActionsOIDCRole"
-
   assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect = "Allow"
-      Principal = {
-        Federated = aws_iam_openid_connect_provider.github.arn
-      }
-      Action = "sts:AssumeRoleWithWebIdentity"
-      Condition = {
-        StringLike = {
-          "token.actions.githubusercontent.com:sub" = "repo:abbysac/billing:*"
-        }
-      }
-    }]
-  })
-}
-
-# 3. Permissions for the role
-resource "aws_iam_role_policy" "github_oidc_policy" {
-  name = "GitHubOIDCPolicy"
-  role = aws_iam_role.github_oidc_role.id
-
-  policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
         Effect = "Allow"
-        Action = [
-          "s3:ListAllMyBuckets",
-          "s3:GetObject",
-          "sts:GetCallerIdentity",
-          
-        ]
-        Resource = "arn:aws:iam::224761220970:oidc-provider/token.actions.githubusercontent.com"
-      },
-      {
-        Effect = "Allow"
-        Action = [
-          "iam:GetOpenIDConnectProvider",
-          "iam:GetRole",
-          "iam:ListRolePolicies",
-          "iam:GetRolePolicy"
-        ]
-        Resource = [
-            
-            "arn:aws:iam::224761220970:oidc-provider/token.actions.githubusercontent.com"
-        ]
+        Principal = {
+          Federated = "arn:aws:iam::224761220970:oidc-provider/token.actions.githubusercontent.com"
+        }
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+          }
+          StringLike = {
+            "token.actions.githubusercontent.com:sub" = "repo:abbysac/billing:*"
+          }
+        }
       }
     ]
   })
+
+  # Add this inline policy to grant the necessary permission
+  inline_policy {
+    name = "list-role-policies"
+    policy = jsonencode({
+      Version = "2012-10-17"
+      Statement = [
+        {
+          Effect   = "Allow"
+          Action   = "iam:ListRolePolicies"
+          Resource = "arn:aws:iam::224761220970:role/GitHubActionsOIDCRole"
+        }
+      ]
+    })
+  }
+
+  # Include any other existing policies or configurations here
 }
