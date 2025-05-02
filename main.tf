3fabeb1b433638c0042a99b867d0c5a623fd4f15
abbysac <abbysac@gmail.com>
2025-05-02 10:27:14 -0400

    gha policy

--- main.tf
+++ main.tf
@@ -106,7 +106,8 @@ resource "aws_iam_role_policy" "github_oidc_policy" {
         Action = [
           "iam:GetOpenIDConnectProvider",
           "iam:GetRole",
-          "iam:ListRolePolicies"
+          "iam:ListRolePolicies",
+          "iam:GetRolePolicy"
         ]
         Resource = "arn:aws:iam::224761220970:oidc-provider/token.actions.githubusercontent.com"
       }
