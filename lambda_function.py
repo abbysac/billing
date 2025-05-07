9285aa68aee296dd6c5cec912a0af5fa683e419f
abbysac <abbysac@gmail.com>
2025-05-07 01:59:30 -0400

    lambda

--- lambda_function.py
+++ lambda_function.py
@@ -4,28 +4,25 @@ import os
 
 ses = boto3.client('ses')
 
-SENDER_EMAIL = os.environ['camleous@yahoo.com']
-RECIPIENT_EMAILS = os.environ['camleous@yahoo.com'].split(',')  # comma-separated string
+SENDER_EMAIL = os.environ['SENDER_EMAIL']
+RECIPIENT_EMAILS = os.environ['RECIPIENT_EMAILS'].split(',')
 
 def lambda_handler(event, context):
     print("Received event:", json.dumps(event))
 
     for record in event['Records']:
         message = record['Sns']['Message']
-        subject = record['Sns']['Subject'] if 'Subject' in record['Sns'] else 'AWS Budget Alert'
+        subject = record['Sns'].get('Subject', 'AWS Budget Alert')
 
         response = ses.send_email(
             Source=SENDER_EMAIL,
-            Destination={
-                'ToAddresses': RECIPIENT_EMAILS
-            },
+            Destination={'ToAddresses': RECIPIENT_EMAILS},
             Message={
                 'Subject': {'Data': subject},
-                'Body': {
-                    'Text': {'Data': message}
-                }
+                'Body': {'Text': {'Data': message}}
             }
         )
+
         print("SES send_email response:", response)
 
     return {"statusCode": 200, "body": "Email sent"}
