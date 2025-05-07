cd25c24bc92b5927c8c3a96ececedb68e44c2990
abbysac <abbysac@gmail.com>
2025-05-07 00:42:35 -0400

    lambda

---
+++ lambda_function.py
@@ -0,0 +1,31 @@
+import json
+import boto3
+import os
+
+ses = boto3.client('ses')
+
+SENDER_EMAIL = os.environ['abbysac@gmail.com']
+RECIPIENT_EMAILS = os.environ['camleous@yahoo.com'].split(',')  # comma-separated string
+
+def lambda_handler(event, context):
+    print("Received event:", json.dumps(event))
+
+    for record in event['Records']:
+        message = record['Sns']['Message']
+        subject = record['Sns']['Subject'] if 'Subject' in record['Sns'] else 'AWS Budget Alert'
+
+        response = ses.send_email(
+            Source=SENDER_EMAIL,
+            Destination={
+                'ToAddresses': RECIPIENT_EMAILS
+            },
+            Message={
+                'Subject': {'Data': subject},
+                'Body': {
+                    'Text': {'Data': message}
+                }
+            }
+        )
+        print("SES send_email response:", response)
+
+    return {"statusCode": 200, "body": "Email sent"}
--- main.tf
+++ main.tf
@@ -40,7 +40,7 @@ resource "aws_budgets_budget" "budget_notification" {
 
   notification {
     comparison_operator        = "GREATER_THAN"
-    threshold                  = 100
+    threshold                  = 80
     threshold_type             = "PERCENTAGE"
     notification_type          = each.value.Alert4Trigger
     # subscriber_email_addresses = [each.value.Alert4Emails]
@@ -141,3 +141,111 @@ resource "aws_iam_role_policy_attachment" "budgets_view_attachment" {
 data "aws_caller_identity" "current" {}
 
 # account_id = data.aws_caller_identity.current.account_id
+
+
+resource "aws_iam_policy" "policy" {
+  name        = "budget_sns_gha_policy"
+  path        = "/"
+  description = "budget_sns_gha_policy"
+
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Sid" : "SNSFullAccess",
+        "Effect" : "Allow",
+        "Action" : "sns:*",
+        "Resource" : "*"
+      },
+      { "Sid" : "SESFullAccess"
+        "Effect" : "Allow",
+        "Action" : "ses:SendEmail",
+        "Resource" : "*"
+      },
+      {
+        "Sid" : "SMSAccessViaSNS",
+        "Effect" : "Allow",
+        "Action" : [
+          "sms-voice:DescribeVerifiedDestinationNumbers",
+          "sms-voice:CreateVerifiedDestinationNumber",
+          "sms-voice:SendDestinationNumberVerificationCode",
+          "sms-voice:SendTextMessage",
+          "sms-voice:DeleteVerifiedDestinationNumber",
+          "sms-voice:VerifyDestinationNumber",
+          "sms-voice:DescribeAccountAttributes",
+          "sms-voice:DescribeSpendLimits",
+          "sms-voice:DescribePhoneNumbers",
+          "sms-voice:SetTextMessageSpendLimitOverride",
+          "sms-voice:DescribeOptedOutNumbers",
+          "sms-voice:DeleteOptedOutNumber"
+        ],
+        "Resource" : "*",
+        "Condition" : {
+          "StringEquals" : {
+            "aws:CalledViaLast" : [
+              "sns.amazonaws.com"
+            ]
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy_attachment" "ec2_gha_attach" {
+  name       = "budget_policy_attachment"
+  roles      = [aws_iam_role.lambda_role.name]
+  policy_arn = aws_iam_policy.policy.arn
+}
+
+
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda_budget_gha_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      },
+      {
+        "Effect" : "Allow",
+        "Principal" : {
+          "Service" : "scheduler.amazonaws.com"
+        },
+        "Action" : "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_lambda_function" "test_lambda" {
+  # If the file is not in the current working directory you will need to include a
+  # path.module in the filename.
+  filename      = "lambda_function.zip"
+  function_name = "budget_update_gha_alert"
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "lambda_function.lambda_handler"
+
+  source_code_hash = data.archive_file.lambda.output_base64sha256
+
+  runtime = "python3.11"
+
+  environment {
+    variables = {
+      foo = "bar"
+    }
+  }
+}
+resource "aws_cloudwatch_log_group" "lambda" {
+  name = "/aws/lambda/budget_update_gha_alert"
+}
+
+
+# resource "aws_s3_bucket" "my_bucket" {
+#   bucket = "my-import-bucket234"
+# }
