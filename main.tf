e47f0621d43bac75da2a0c39b1e423b01a3d6c0a
abbysac <abbysac@gmail.com>
2025-05-08 14:00:36 -0400

    csvdata

--- main.tf
+++ main.tf
@@ -46,7 +46,7 @@ resource "aws_budgets_budget" "budget_notification" {
 
   notification {
     comparison_operator        = "GREATER_THAN"
-    threshold                  = 80
+    threshold                  = 70
     threshold_type             = "PERCENTAGE"
     notification_type          = each.value.Alert4Trigger
     # subscriber_email_addresses = [each.value.Alert4Emails]
