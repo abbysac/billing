f797f4f8a3c9c3c4850f837b0e105890684f8544
abbysac <abbysac@gmail.com>
2025-05-08 14:22:57 -0400

    csvdata

--- main.tf
+++ main.tf
@@ -24,36 +24,36 @@ resource "aws_budgets_budget" "budget_notification" {
     
   }
 
-  notification {
-    comparison_operator        = "GREATER_THAN"
-    threshold                  = 100
-    threshold_type             = "PERCENTAGE"
-    notification_type          = each.value.Alert2Trigger
-    # subscriber_email_addresses = [each.value.Alert2Emails]
-    subscriber_sns_topic_arns  = ["arn:aws:sns:us-east-1:224761220970:budget-updates-topic"]
+  # notification {
+  #   comparison_operator        = "GREATER_THAN"
+  #   threshold                  = 100
+  #   threshold_type             = "PERCENTAGE"
+  #   notification_type          = each.value.Alert2Trigger
+  #   # subscriber_email_addresses = [each.value.Alert2Emails]
+  #   subscriber_sns_topic_arns  = ["arn:aws:sns:us-east-1:224761220970:budget-updates-topic"]
     
-  }
+  # }
 
-  notification {
-    comparison_operator        = "GREATER_THAN"
-    threshold                  = 100
-    threshold_type             = "PERCENTAGE"
-    notification_type          = each.value.Alert3Trigger
-    # subscriber_email_addresses = [each.value.Alert3Emails]
-    subscriber_sns_topic_arns  = ["arn:aws:sns:us-east-1:224761220970:budget-updates-topic"]
+  # notification {
+  #   comparison_operator        = "GREATER_THAN"
+  #   threshold                  = 100
+  #   threshold_type             = "PERCENTAGE"
+  #   notification_type          = each.value.Alert3Trigger
+  #   # subscriber_email_addresses = [each.value.Alert3Emails]
+  #   subscriber_sns_topic_arns  = ["arn:aws:sns:us-east-1:224761220970:budget-updates-topic"]
     
-  }
+  # }
 
-  notification {
-    comparison_operator        = "GREATER_THAN"
-    threshold                  = 70
-    threshold_type             = "PERCENTAGE"
-    notification_type          = each.value.Alert4Trigger
-    # subscriber_email_addresses = [each.value.Alert4Emails]
-    subscriber_sns_topic_arns  = ["arn:aws:sns:us-east-1:224761220970:budget-updates-topic"]
+  # notification {
+  #   comparison_operator        = "GREATER_THAN"
+  #   threshold                  = 70
+  #   threshold_type             = "PERCENTAGE"
+  #   notification_type          = each.value.Alert4Trigger
+  #   # subscriber_email_addresses = [each.value.Alert4Emails]
+  #   subscriber_sns_topic_arns  = ["arn:aws:sns:us-east-1:224761220970:budget-updates-topic"]
     
   
-  }
+  # }
 
 }
 
