3b77545d717ebecf31ec24a4dd7546614fece4b1
abbysac <abbysac@gmail.com>
2025-05-26 03:15:20 -0400

    list  role

--- main.tf
+++ main.tf
@@ -242,21 +242,21 @@ resource "aws_iam_policy" "policy" {
          "arn:aws:lambda:us-east-1:224761220970:function:budget_update_gha_alert"
          
         ]
-      },
-      { "Sid" : "OidcAccess",
-        "Effect" : "Allow",
-        "Action" : [
-          "iam:GetPolicy",
-          "iam:GetPolicyVersion"
-        ], 
-        "Resource" : [
-          "arn:aws:iam::224761220970:role/GitHubActionsOIDCRole",
-          "arn:aws:budgets::224761220970:budget/ABC Operations DEV Account Overall Budget",
-          "arn:aws:budgets::224761220970:budget/ABC Operations PROD Account Overall Budget",
-          "arn:aws:lambda:us-east-1:224761220970:function:budget_update_gha_alert",
-          "arn:aws:iam::224761220970:oidc-provider/token.actions.githubusercontent.com"
-        ]
       }
+      # { "Sid" : "OidcAccess",
+      #   "Effect" : "Allow",
+      #   "Action" : [
+      #     "iam:GetPolicy",
+      #     "iam:GetPolicyVersion"
+      #   ], 
+      #   "Resource" : [
+      #     "arn:aws:iam::224761220970:role/GitHubActionsOIDCRole",
+      #     "arn:aws:budgets::224761220970:budget/ABC Operations DEV Account Overall Budget",
+      #     "arn:aws:budgets::224761220970:budget/ABC Operations PROD Account Overall Budget",
+      #     "arn:aws:lambda:us-east-1:224761220970:function:budget_update_gha_alert",
+      #     "arn:aws:iam::224761220970:oidc-provider/token.actions.githubusercontent.com"
+      #   ]
+      # }
     ]
   })
 }
