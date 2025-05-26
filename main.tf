617661102c77eec7e3d00da88b2391564037518a
abbysac <abbysac@gmail.com>
2025-05-26 02:49:43 -0400

    github role

--- main.tf
+++ main.tf
@@ -131,7 +131,8 @@ resource "aws_iam_role_policy" "github_oidc_policy" {
         Resource = [
             "arn:aws:iam::224761220970:role/GitHubActionsOIDCRole",      
             "arn:aws:iam::224761220970:oidc-provider/token.actions.githubusercontent.com",
-            "arn:aws:lambda:us-east-1:224761220970:function:budget_update_gha_alert"
+            "arn:aws:lambda:us-east-1:224761220970:function:budget_update_gha_alert",
+            "arn:aws:iam::224761220970:policy/budgets-view-policy"
         ]
       },
       {
@@ -244,7 +245,12 @@ resource "aws_iam_policy" "policy" {
           "iam:GetPolicy",
           "iam:GetPolicyVersion"
         ], 
-        "Resource" : "arn:aws:iam::224761220970:role/GitHubActionsOIDCRole"
+        "Resource" : [
+          "arn:aws:iam::224761220970:role/GitHubActionsOIDCRole",
+          "arn:aws:budgets::224761220970:budget/ABC Operations DEV Account Overall Budget",
+          "arn:aws:budgets::224761220970:budget/ABC Operations PROD Account Overall Budget",
+          "arn:aws:lambda:us-east-1:224761220970:function:budget_update_gha_alert"
+        ]
       }
     ]
   })
