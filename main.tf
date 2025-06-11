2cab5f8d0ace981f78f0c8d53d34c426f7ed8c47
camleous <camleous@yahoo.com>
2025-06-11 00:31:48 -0400

    new

--- main.tf
+++ main.tf
@@ -145,7 +145,7 @@ resource "aws_iam_role_policy" "github_oidc_policy" {
           "iam:ListAttachedRolePolicies",
           "iam:ListEntitiesForPolicy"
         ],
-        "Resource" : "arn:aws:iam::224761220970:role/lambda_budget_gha_role"
+        "Resource" : "arn:aws:iam::224761220970:policy/budget_sns_gha_policy"
       }
 
     ]
@@ -267,8 +267,8 @@ resource "aws_iam_policy" "policy" {
           "arn:aws:budgets::224761220970:budget/ABC Operations DEV Account Overall Budget",
           "arn:aws:budgets::224761220970:budget/ABC Operations PROD Account Overall Budget",
           "arn:aws:lambda:us-east-1:224761220970:function:budget_update_gha_alert",
-          "arn:aws:iam::224761220970:oidc-provider/token.actions.githubusercontent.com",
-          "arn:aws:iam::224761220970:policy/budget_sns_gha_policy"
+          "arn:aws:iam::224761220970:oidc-provider/token.actions.githubusercontent.com"
+
 
         ]
       }
