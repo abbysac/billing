f1e8d4f39028870e5349bc05f2affad873be3da4
camleous <camleous@yahoo.com>
2025-06-11 22:12:02 -0400

    version

--- main.tf
+++ main.tf
@@ -342,7 +342,7 @@ resource "aws_iam_policy" "policy" {
         "Effect" : "Allow",
         "Action" : "ssm:StartAutomationExecution",
         "Resource" : [
-          "arn:aws:ssm:us-east-1:224761220970:automation-definition/budget_update_gha_alert:*",
+          # "arn:aws:ssm:us-east-1:224761220970:automation-definition/budget_update_gha_alert:*",
           "arn:aws:ssm:us-east-1:224761220970:automation-definition/budget_update_gha_alert:$DEFAULT"
         ]
       }
