71c41617f4ba7a3041dc4adcad089b27b1040a98
camleous <camleous@yahoo.com>
2025-06-22 17:43:14 -0400

    ssm param

--- main.tf
+++ main.tf
@@ -844,22 +844,22 @@ EOF
   })
 }
 
-# Trigger SSM on CSV change
-# resource "null_resource" "trigger_ssm_on_csv_change" {
-#   triggers = {
-#     csv_hash = local.csv_hash
-#   }
+#Trigger SSM on CSV change
+resource "null_resource" "trigger_ssm_on_csv_change" {
+  triggers = {
+    csv_hash = local.csv_hash
+  }
 
-#   provisioner "local-exec" {
-#     command = <<-EOT
-#       aws ssm start-automation-execution \
-#         --document-name "budget_update_gha_alert" \
-#         --parameters '{"TargetAccountId":"224761220970","BudgetName":${jsonencode([for item in local.csvfld : item.BudgetName])},"SnsTopicArn":"arn:aws:sns:us-east-1:224761220970:budget-updates-topic","Message":"Budget threshold exceeded alert"}' \
-#         --region us-east-1
-#     EOT
-#   }
+  provisioner "local-exec" {
+    command = <<-EOT
+      aws ssm start-automation-execution \
+        --document-name "budget_update_gha_alert" \
+        --parameters '{"TargetAccountId":"224761220970","BudgetName":${jsonencode([for item in local.csvfld : item.BudgetName])},"SnsTopicArn":"arn:aws:sns:us-east-1:224761220970:budget-updates-topic","Message":"Budget threshold exceeded alert"}' \
+        --region us-east-1
+    EOT
+  }
 
-#   depends_on = [aws_ssm_document.invoke_central_lambda]
-# }
+  depends_on = [aws_ssm_document.invoke_central_lambda]
+}
 
 # AWS Budgets configuration
