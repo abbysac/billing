059cb6e5af160baea1fdc42554bada86269ee5c9
camleous <camleous@yahoo.com>
2025-06-23 17:19:20 -0400

    ssm doc

--- main.tf
+++ main.tf
@@ -167,7 +167,8 @@ resource "aws_iam_role_policy" "github_oidc_policy" {
           "ssm:UpdateDocumentDefaultVersion",
           "ssm:CreateDocument",
           "lambda:UpdateFunctionConfiguration",
-          "ssm:GetParameter"
+          "ssm:GetParameter",
+          "ssm:StartAutomationExecution"
 
         ],
         "Resource" : [
