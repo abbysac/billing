6eee3c7b6b82e6323d2dc3ce813ef57c0ff2dd06
camleous <camleous@yahoo.com>
2025-06-24 00:59:04 -0400

    ssm trigger

--- main.tf
+++ main.tf
@@ -851,7 +851,7 @@ EOF
 resource "null_resource" "trigger_ssm_on_threshold" {
   # Trigger the resource when the threshold condition changes
   triggers = {
-    threshold_status = alert_trigger
+    threshold_status = threshold
   }
 
   # Execute SSM document only when threshold is reached
