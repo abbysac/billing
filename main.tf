77b0b74473df0bd8488bd15a5baa1bb1893809df
camleous <camleous@yahoo.com>
2025-07-02 17:42:58 -0400

    test

--- main.tf
+++ main.tf
@@ -402,7 +402,8 @@ resource "aws_lambda_function" "test_lambda" {
   filename      = "lambda_function.zip"
   function_name = "budget_update_gha_alert"
   role          = aws_iam_role.lambda_role.arn
-  handler       = "lambda_function.lambda_handler"
+  # handler       = "lambda_function.lambda_handler"
+  handler = lambda_function.process_sns_message
 
   source_code_hash = data.archive_file.lambda.output_base64sha256
 
