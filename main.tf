c57658295d9aaf5a395be3433d4a6bdb157013f4
camleous <camleous@yahoo.com>
2025-07-03 15:49:33 -0400

    test

--- main.tf
+++ main.tf
@@ -412,7 +412,7 @@ resource "aws_lambda_function" "test_lambda" {
 
   source_code_hash = data.archive_file.lambda.output_base64sha256
 
-  runtime = "python3.12"
+  runtime = "python3.11"
 
   environment {
     variables = {
