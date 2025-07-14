3978eb8dc4e1babff85b4468767dd6548366c7cc
camleous <camleous@yahoo.com>
2025-07-14 12:23:18 -0400

    SSM document

--- main.tf
+++ main.tf
@@ -739,7 +739,7 @@ resource "aws_ssm_document" "invoke_central_lambda" {
           { Selector = "$.PublishToSNS.MessageId" },
           { Type = "String" },
           { Name = "Status" },
-          { Selector = "$.InstanceId" },
+          { Selector = "$.PublishToSNS" },
           { Type = "String" }
         ]
       },
